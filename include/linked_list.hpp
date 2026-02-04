e64c79579bfdd9178162a35e038237c918b24855
snow030 <kuekzhengyu@gmail.com>
2026-02-04 17:23:05 +0800

    feat: new project idea & linked list struct

--- include/linked_list.hpp
+++ include/linked_list.hpp
@@ -4,28 +4,21 @@
 #include <string>
 
 namespace fms::llist {
-    struct BookItem {
-        std::string psg_id;
-        std::string psg_name;
-        int seat_row;
-        char seat_col;
-        char seat_class;
-        BookItem *prev;
-        BookItem *next;
-    };
-
-    extern BookItem *book_head;
-    extern BookItem *book_tail;
-
-    void push_book(BookItem *booking);
+    struct BookItem;
+    struct BookList;
+    struct Seat;
+    struct SeatRow;
+    struct SeatMap;
 
     void setup();
 
     void reserve();
     void cancel();
+    void auto_fill();
     void lookup();
     void print_seat();
     void print_passenger();
+    void dispatch();
 
     void teardown();
 }
--- menu.cpp
+++ menu.cpp
@@ -6,7 +6,8 @@
 
 using std::cout,
       std::cin,
-      std::endl;
+      std::endl,
+      std::string;
 
 using namespace std::chrono;
 
@@ -54,30 +55,16 @@ namespace fms::menu {
 
     void array_m(void *next_m) {
         auto &next_fn = *(void (**)(void *)) next_m;
-        cout << "Array-based Implementation Menu:" << endl
-             << "    1. Reservation (Allocate Seat)" << endl
-             << "    2. Cancellation (Remove Passenger)" << endl
-             << "    3. Seat Lookup (Use passengerId)" << endl
-             << "    4. Print Seating Chart" << endl
-             << "    5. Print Passenger Manifest" << endl
-             << "    x. Return Main Menu" << endl << endl;
 
-        char choice;
-        while (true) {
-            cout << "Select a feature: [   ]\033[3D";
-            cin >> choice;
-
-            if ('0' < choice && choice < '6' || choice == 'x') break;
-            cout << "\033[91m** Invalid Option ! **\033[m" << endl;
-        }
-        cout << endl;
-
+        char choice = base_m("Array-based Implementation Menu:");
         switch (choice) {
             case '1': array::reserve(); break;
             case '2': array::cancel(); break;
-            case '3': array::lookup(); break;
-            case '4': array::print_seat(); break;
-            case '5': array::print_passenger(); break;
+            case '3': array::auto_fill(); break;
+            case '4': array::lookup(); break;
+            case '5': array::print_seat(); break;
+            case '6': array::print_passenger(); break;
+            case '7': array::dispatch(); break;
             case 'x': next_fn = main_m; return;
         }
 
@@ -86,33 +73,42 @@ namespace fms::menu {
 
     void llist_m(void *next_m) {
         auto &next_fn = *(void (**)(void *)) next_m;
-        cout << "Linked List-based Implementation Menu:" << endl
+
+        char choice = base_m("Linked List-based Implementation Menu:");
+        switch (choice) {
+            case '1': llist::reserve(); break;
+            case '2': llist::cancel(); break;
+            case '3': llist::auto_fill(); break;
+            case '4': llist::lookup(); break;
+            case '5': llist::print_seat(); break;
+            case '6': llist::print_passenger(); break;
+            case '7': llist::dispatch(); break;
+            case 'x': next_fn = main_m; return;
+        }
+
+        next_fn = llist_m;
+    }
+
+    char base_m(string title) {
+        cout << title << endl
              << "    1. Reservation (Allocate Seat)" << endl
              << "    2. Cancellation (Remove Passenger)" << endl
-             << "    3. Seat Lookup (Use passengerId)" << endl
-             << "    4. Print Seating Chart" << endl
-             << "    5. Print Passenger Manifest" << endl
-             << "    x. Return Main Menu" << endl << endl;
+             << "    3. Auto Filling" << endl
+             << "    4. Seat Lookup (Use passengerId)" << endl
+             << "    5. Print Seating Chart" << endl
+             << "    6. Print Passenger Manifest" << endl
+             << "    7. Dispatch Now" << endl
+             << "    x. Return Main Menu (Reset)" << endl << endl;
 
         char choice;
         while (true) {
             cout << "Select a feature: [   ]\033[3D";
             cin >> choice;
 
-            if ('0' < choice && choice < '6' || choice == 'x') break;
+            if ('0' < choice && choice < '8' || choice == 'x') break;
             cout << "\033[91m** Invalid Option ! **\033[m" << endl;
         }
         cout << endl;
-
-        switch (choice) {
-            case '1': llist::reserve(); break;
-            case '2': llist::cancel(); break;
-            case '3': llist::lookup(); break;
-            case '4': llist::print_seat(); break;
-            case '5': llist::print_passenger(); break;
-            case 'x': llist::teardown(); next_fn = main_m; return;
-        }
-
-        next_fn = llist_m;
+        return choice;
     }
 }
